e75c026405e67e14760c7cd5f2a45e6b10777e03
Herrick Fang <herrickfang@gmail.com>
2020-12-19 15:33:31 -0800

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  siteMetadata: {
+    title: "gousse",
+  },
+  plugins: [
+    "gatsby-plugin-styled-components",
+    {
+      resolve: "gatsby-plugin-google-analytics",
+      options: {
+        trackingId: "",
+      },
+    },
+    "gatsby-plugin-sharp",
+    "gatsby-plugin-sitemap",
+    "gatsby-transformer-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "images",
+        path: "./src/images/",
+      },
+      __key: "images",
+    },
+  ],
+};
